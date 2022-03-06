bc478431e68c4f41a68c67934efc2971dc41c130
Mahesh Birajdar <98763822+MaheshBBirajdar@users.noreply.github.com>
2022-03-06 14:14:48 +0530

    02 march  Assignment

--- assignment.py
+++ assignment.py
@@ -1,9 +1,17 @@
-str1 = "what we think we become we"
-p = "we"
-a = -1
-c = str1.count("we")
-for each in range(c):
-	a = str1.find(p,a+1)
-    print (each)
-	print (a)
-	
+
+def fun1():
+    a,b= 20,50
+    c=a+b
+    if c<50:
+        print("ok")
+    else:
+        print("critical")
+    
+fun1()
+
+
+
+
+
+
+
